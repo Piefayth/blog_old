31337885a48283d9a66c38e57d175da1667fbd9e
Sean Ryan <piefayth@gmail.com>
2019-03-10 22:23:39 -0500

    potentially better resolution idk

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,6 +1,7 @@
 import ActionTypes from '../constants/actionTypes'
 import { createAction } from 'redux-actions'
 import axios from 'axios'
+import env from '../utils/env'
 
 
 export const getPostSuccess = post => ({
@@ -14,7 +15,14 @@ export const getPostError = error => ({
 })
 
 export const getPost = (id) => (dispatch) => {
-    axios.get(`/static/posts/${id}.md`)
+    let uri
+    if (env.env === 'development') {
+        uri = env.devUrl
+    } else {
+        uri = env.prodUrl
+    }
+    
+    axios.get(`${uri}${id}.md`)
         .then(ok => (dispatch(getPostSuccess(ok.data))))
         .catch(error => (dispatch(getPostError(error))))
 }
---
+++ src/utils/env.js
@@ -0,0 +1,10 @@
+
+const prodUrl = process.env.REACT_APP_PUBLIC_URI
+const devUrl = process.env.REACT_APP_DEV_URI
+const env = process.env.NODE_ENV
+
+export default {
+    prodUrl,
+    devUrl,
+    env
+}
